075e82a9b52899055b012a82bb3fb1c71d1bd5cc
jonatan85 <114008152+jonatan85@users.noreply.github.com>
2023-07-18 16:15:55 +0200

    footer-nav

--- src/components/nav/Nav.jsx
+++ src/components/nav/Nav.jsx
@@ -1,19 +1,27 @@
-
+import Image from "next/image";
 import ActiveLink from "../activeLink/ActiveLink";
-import styles from './nav.module.css'
-import Carrito from "@/app/carrito/page";
+import styles from './nav.module.css';
+import image from '../../public/img/home.png';
 
 export default function Nav() {
   return (
-   <>
+   <>   
+    <div>
         <nav className={styles.nav}>
-            <ActiveLink text='Home' href='/'/>
-            <ActiveLink text='Platos' href='/platos'/>
-            <ActiveLink text='Ingredientes' href='/ingredientes'/>
-            <ActiveLink text='Añade tus platos' href='/crud'/>
-            <ActiveLink text='Añade Ingredientes' href='/crudIngredientes'/>
-            <ActiveLink text='Nosotros' href='/nosotros'/>
+            <div className={styles.iconContainer}>
+            <a href="/">
+              <Image className={styles.icon} src={image} alt="home"/>
+            </a>
+            </div>
+            <div className={styles.linksContainer}>
+              <ActiveLink text='Home' href='/'></ActiveLink>
+              <ActiveLink text='Platos' href='/platos'/>
+              <ActiveLink text='Ingredientes' href='/ingredientes'/>
+              <ActiveLink text='Añade tus platos' href='/crud'/>
+              <ActiveLink text='Añade Ingredientes' href='/crudIngredientes'/>
+            </div>
         </nav>
+    </div>
    </>
   )
 }
